2a1a47acdf87b0100f05c0df53d68b6652d0cac0
Joe Butler <joe@joebutler.dev>
2021-02-23 14:56:32 +1300

    Add initial db migration and connection stuff

---
+++ src/Commander.API/Data/CommanderContext.cs
@@ -0,0 +1,15 @@
+using Commander.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.API.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
--- src/Commander.API/Models/Command.cs
+++ src/Commander.API/Models/Command.cs
@@ -1,10 +1,19 @@
-﻿namespace Commander.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Commander.API.Models
 {
     public class Command
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Platform { get; set; }
     }
 }
