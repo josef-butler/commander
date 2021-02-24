0180c4681fb2d0c08fa16f6f7e96ccab4db96e7e
Joe Butler <joe@joebutler.dev>
2021-02-25 11:56:27 +1300

    Annotate the create Dto to improve error handling when the client is missing fields from the request

--- src/Commander.API/Dtos/CommandCreateDto.cs
+++ src/Commander.API/Dtos/CommandCreateDto.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.API.Dtos
 {
     // Structure of the object that clients must send to create a command in the db
+    // Required annotations are included so that the API can return useful error messaging to the client (instead of a 500 server error)
     public class CommandCreateDto
     {
+        [Required]
         public string HowTo { get; set; }
-
+        
+        [Required]
         public string Line { get; set; }
-
+        
+        [Required]
         public string Platform { get; set; }
     }
 }
